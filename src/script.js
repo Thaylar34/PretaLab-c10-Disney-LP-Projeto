230670261387c8a06349136edda064d959372953
Thayla Ramos <thaylar34@gmail.com>
2024-06-02 23:02:50 -0300

    funcionalidade de contar pontuação

--- src/script.js
+++ src/script.js
@@ -1,12 +1,28 @@
-let numeroAleatorio = 0;
+let numeroAleatorio = 50;
 let tentativas = 0;
 
 function jogoDeAdivinhacao() {
     const palpiteDigitado = pegarPalpiteDigitado();
     
     if (!palpiteDigitado) {
-        alert("Digite um valor válido!")
+        alert("Digite um valor válido!");
+        return;
     }
+
+    if (palpiteDigitado === numeroAleatorio) {
+        alert("Parabéns, você adivinhou!")
+    } else if (palpiteDigitado > numeroAleatorio) {
+        tentativas++;
+        atualizarFeedback("Esse numero é muito alto. Tente novamente");
+    } else if(palpiteDigitado < numeroAleatorio) {
+        tentativas++;
+        atualizarFeedback("Esse numero é muito alto. Tente novamente");
+    }  
+
+    const novaPontuacao = 100 - (tentativas * 10);
+    atualizarPontuacao(novaPontuacao);
+
+
 }
 
 function reiniciarJogo() {
